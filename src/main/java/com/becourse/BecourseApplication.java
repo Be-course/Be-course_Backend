7d0501350c33a156e3f15ddebe546c58294eea59
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-12-26 10:50:33 +0900

    fixed untracked files

---
+++ src/main/java/com/becourse/BecourseApplication.java
@@ -0,0 +1,13 @@
+package com.becourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BecourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/becourse/BecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
